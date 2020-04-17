a592d3ebbcf0e8f3fd53f1194dd2132c85d4a4cb
stephen pesce <stevepesce879@gmail.com>
2020-04-17 05:04:57 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+def using_push(my_array, my_string)
+  my_array.push(my_string)
+end
+def using_unshift(my_array, my_string)
+
+end
+def using_pop
+  
+end
+def pop_with_args
+  
+end
+def using_shift
+  
+end
+def shift_with_args
+  
+  
